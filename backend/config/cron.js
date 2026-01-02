2dd0bcb4e655ee65c2cddc1d838ab5cf44360049
Kaushik Mandal <koushik369mondal@gmail.com>
2026-01-02 18:30:01 +0530

    cron job configuration for improved readability and maintainability

--- backend/config/cron.js
+++ backend/config/cron.js
@@ -1,13 +1,19 @@
 import cron from "cron";
 import https from "https";
 
-const job = new cron.CronJob("*/14 * * * *", function () {
-    https
-        .get(process.env.API_URL, (res) => {
-            if (res.statusCode === 200) console.log("GET request sent successfully");
-            else console.log("GET request failed", res.statusCode);
-        })
-        .on("error", (e) => console.error("Error while sending request", e));
-});
+const job = new cron.CronJob(
+    "*/14 * * * *", // Run every 14 minutes
+    function () {
+        https
+            .get(process.env.API_URL, (res) => {
+                if (res.statusCode === 200) console.log("GET request sent successfully");
+                else console.log("GET request failed", res.statusCode);
+            })
+            .on("error", (e) => console.error("Error while sending request", e));
+    },
+    null, // onComplete
+    false, // start (false = don't start immediately, will be started manually in server.js)
+    "America/New_York" // timeZone
+);
 
 export default job;
